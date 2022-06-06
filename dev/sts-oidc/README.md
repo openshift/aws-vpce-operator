5b6e919e2fa79545bea3fe12118c81691ac97af0
Michael Shen <mshen@redhat.com>
2022-06-06 15:21:21 -0400

    Add docs for learning about STS/OIDC
    Signed-off-by: Michael Shen <mshen@redhat.com>

---
+++ dev/sts-oidc/README.md
@@ -0,0 +1,62 @@
+# AWS STS CLI Debugging
+
+This example requires an STS ROSA cluster and can be used to run arbitrary AWS CLI commands from a pod on the ROSA cluster that is restricted by an IAM policy.
+
+This is more for exploration/demo purposes on how the STS/OIDC flow between ROSA and AWS works. Often it is more practical to iterate with the running operator directly.
+
+## Prerequisites
+
+* A running STS ROSA cluster
+* terraform
+* osdtcl
+* aws
+
+## Creating
+
+1. Set convenience environment variables:
+
+    ```bash
+    AWS_ACCOUNT_ID=
+    ROSA_INTERNAL_ID=
+    ```
+
+2. Get AWS CLI credentials:
+
+    ```bash
+    export $(osdctl account cli -i ${AWS_ACCOUNT_ID} -p osd-staging-2 -o env | xargs)
+    ```
+
+3. Provision the "iam-test-aws" AWS IAM role which will allow pods run by the "rosa-to-iam" K8s serviceaccount to assume it via the ROSA cluster's OIDC provider:
+
+    ```bash
+    terraform init
+    terraform apply -var aws_account_id="${AWS_ACCOUNT_ID}" -var rosa_internal_id="${ROSA_INTERNAL_ID}"
+    ```
+
+4. Ensure the current Kubernetes context is set to your STS ROSA cluster and then apply the manifests:
+
+    ```bash
+    oc process -f aws-sts-debug.tmpl.yml AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID}" | oc create -f -
+    ```
+
+5. ???
+
+6. Profit
+
+    ```bash
+    oc exec -it -n aws-sts-debug awscli-debug -- bash
+    aws sts get-caller-identity
+    ```
+
+## Cleanup
+
+1. The Terraform resources can be cleaned up if desired (no resources costing money get provisioned)
+
+    ```bash
+    terraform destroy -var aws_account_id="${AWS_ACCOUNT_ID}" -var rosa_internal_id="${ROSA_INTERNAL_ID}"
+    ```
+
+## References
+
+* [Fine-grained IAM roles for ROSA workloads with STS](https://aws.amazon.com/blogs/containers/fine-grained-iam-roles-for-red-hat-openshift-service-on-aws-rosa-workloads-with-sts/)
+* [How does ROSA use STS?](https://cloud.redhat.com/blog/what-is-aws-sts-and-how-does-red-hat-openshift-service-on-aws-rosa-use-sts)
---
+++ dev/sts-oidc/main.tf
@@ -0,0 +1,117 @@
+provider "aws" {
+  region  = "us-east-2"
+  profile = "osd-staging-2"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.aws_account_id}:role/OrganizationAccountAccessRole"
+  }
+
+  default_tags {
+    tags = {
+      owner = "terraform"
+    }
+  }
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "AWS Account ID to deploy this into"
+}
+
+variable "rosa_internal_id" {
+  type        = string
+  description = "Internal ID of the ROSA cluster"
+}
+
+locals {
+  oidc_provider        = "rh-oidc-staging.s3.us-east-1.amazonaws.com/${var.rosa_internal_id}"
+  namespace            = "awscli-sts-debug"
+  service_account_name = "rosa-to-iam"
+}
+
+resource "aws_iam_role" "iam_test" {
+  name               = "iam-test-aws"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json
+}
+
+data "aws_iam_policy_document" "trust_policy" {
+  version = "2012-10-17"
+
+  statement {
+    sid     = "OpenshiftStsOidcTrustPolicy"
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${var.aws_account_id}:oidc-provider/${local.oidc_provider}"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.oidc_provider}:sub"
+      values   = ["system:serviceaccount:${local.namespace}:${local.service_account_name}"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${local.oidc_provider}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "iam_test" {
+  version = "2012-10-17"
+
+  statement {
+    sid       = "HelloWorld"
+    effect    = "Allow"
+    actions   = ["sts:GetCallerIdentity"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ManageTags"
+    effect = "Allow"
+    actions = [
+      # Manage tags and filter based on tags
+      "ec2:CreateTags",
+      "ec2:DeleteTags",
+      "ec2:DescribeTags"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ManageSG"
+    effect = "Allow"
+    actions = [
+      # Extract vpc-id by searching for subnets by tag-key
+      "ec2:DescribeSubnets",
+      # Create and manage security group in specific VPC
+      "ec2:CreateSecurityGroup",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DescribeSecurityGroups",
+      # Create and manage security group rules
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:DescribeSecurityGroupRules",
+      # Create and manage a VPC endpoint
+      "ec2:CreateVpcEndpoint",
+      "ec2:DeleteVpcEndpoints",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:ModifyVpcEndpoint"
+    ]
+    resources = ["*"]
+  }
+
+}
+
+resource "aws_iam_policy" "iam_test" {
+  name   = "iam-test-aws"
+  policy = data.aws_iam_policy_document.iam_test.json
+}
+
+resource "aws_iam_role_policy_attachment" "iam_test" {
+  role       = aws_iam_role.iam_test.name
+  policy_arn = aws_iam_policy.iam_test.arn
+}
