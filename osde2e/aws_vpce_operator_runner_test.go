3e2d8bda061b3d59bdc5abc9d04f06522bd3656c
Michael Shen <mshen@redhat.com>
2024-07-15 09:04:56 -0400

    make boilerplate-update
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- osde2e/aws_vpce_operator_runner_test.go
+++ osde2e/aws_vpce_operator_runner_test.go
@@ -22,9 +22,10 @@ const (
 func TestAwsVpceOperator(t *testing.T) {
 	RegisterFailHandler(Fail)
 	suiteConfig, reporterConfig := GinkgoConfiguration()
-	if _, ok := os.LookupEnv("DISABLE_JUNIT_REPORT"); !ok {
+ 	if _, ok := os.LookupEnv("DISABLE_JUNIT_REPORT"); !ok {
 		reporterConfig.JUnitReport = filepath.Join(testResultsDirectory, jUnitOutputFilename)
 	}
 	RunSpecs(t, "Aws Vpce Operator", suiteConfig, reporterConfig)
 
 }
+
